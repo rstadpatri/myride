77383d5a65059186d3a33cdacab2f7b683dfce50
rstadpatri <rstadpatri@gmail.com>
2017-11-20 15:28:28 -0600

    added display buttons

--- myride_gui.cpp
+++ myride_gui.cpp
@@ -48,19 +48,19 @@ private:
 	Button request_submit;
 	Out_box request_summary;
 	Button request_okay;
-<<<<<<< HEAD
 	Button display_customers_all;
 	Button display_customers_neg;
 	Button display_drivers_all;
 	Button display_drivers_rad;
-=======
+	In_box display_drivers_tag;
+	In_box display_drivers_radius;
+	Button display_places_all;
+	Button display_places_withtag;
+	In_box display_places_tag;
 	Menu display_size_menu;
-
->>>>>>> 3fcbbf79fd4149c9cc61c6d604d5fc2590edb42c
 
 	char add_indicator;
 	char remove_indicator;
-
 
 	//function members
 	void hide_menu() {
@@ -282,72 +282,132 @@ private:
 		show_menu();
 	}
 
-<<<<<<< HEAD
-
-			void display_pressed() {
-				//add menu here
-				hide_menu();
-		}
-
-	void display_customers_all() {
-
-=======
 	void display_pressed() {
 		display_size_menu.show();
+		display_customers_all.show();
+		display_customers_neg.show();
+		display_drivers_all.show();
+		display_drivers_tag.show();
+		display_drivers_rad.show();
+		display_drivers_radius.show();
+		display_places_all.show();
+		display_places_withtag.show();
+		display_places_tag.show();
 		hide_menu();
->>>>>>> 3fcbbf79fd4149c9cc61c6d604d5fc2590edb42c
 	}
 
-	void display_customers_neg() {
-
+	void display_customers_all_pressed() {
+		display_customers_all.hide();
+		display_customers_neg.hide();
+		display_drivers_all.hide();
+		display_drivers_tag.hide();
+		display_drivers_rad.hide();
+		display_drivers_radius.hide();
+		display_places_all.hide();
+		display_places_withtag.hide();
+		display_places_tag.hide();
+		display_size_menu.hide();
+		show_menu();
+		//opens display GUI (separate window)
+		//implement logic
 	}
 
-	void display_drivers_all() {
-
+	void display_customers_neg_pressed() {
+		display_customers_all.hide();
+		display_customers_neg.hide();
+		display_drivers_all.hide();
+		display_drivers_tag.hide();
+		display_drivers_rad.hide();
+		display_drivers_radius.hide();
+		display_places_all.hide();
+		display_places_withtag.hide();
+		display_places_tag.hide();
+		display_size_menu.hide();
+		show_menu();
+		//opens display GUI (separate window)
+		//implement logic
 	}
 
-	void display_drivers_rad() {
+	void display_drivers_all_pressed() {
+		display_customers_all.hide();
+		display_customers_neg.hide();
+		display_drivers_all.hide();
+		display_drivers_tag.hide();
+		display_drivers_rad.hide();
+		display_drivers_radius.hide();
+		display_places_all.hide();
+		display_places_withtag.hide();
+		display_places_tag.hide();
+		display_size_menu.hide();
+		show_menu();
+		//opens display GUI (separate window)
+		//implement logic
+	}
 
+	void display_drivers_rad_pressed() {
+		display_customers_all.hide();
+		display_customers_neg.hide();
+		display_drivers_all.hide();
+		display_drivers_tag.hide();
+		display_drivers_rad.hide();
+		display_drivers_radius.hide();
+		display_places_all.hide();
+		display_places_withtag.hide();
+		display_places_tag.hide();
+		display_size_menu.hide();
+		show_menu();
+		//opens display GUI (separate window)
+		//implement logic
+	}
+
+	void display_places_all_pressed() {
+		display_customers_all.hide();
+		display_customers_neg.hide();
+		display_drivers_all.hide();
+		display_drivers_tag.hide();
+		display_drivers_rad.hide();
+		display_drivers_radius.hide();
+		display_places_all.hide();
+		display_places_withtag.hide();
+		display_places_tag.hide();
+		display_size_menu.hide();
+		show_menu();
+		//opens display GUI (separate window)
+		//implement logic
+	}
+
+	void display_places_withtag_pressed() {
+		display_customers_all.hide();
+		display_customers_neg.hide();
+		display_drivers_all.hide();
+		display_drivers_tag.hide();
+		display_drivers_rad.hide();
+		display_drivers_radius.hide();
+		display_places_all.hide();
+		display_places_withtag.hide();
+		display_places_tag.hide();
+		display_size_menu.hide();
+		show_menu();
+		//opens display GUI (separate window)
+		//implement logic
 	}
 
 
-		void menu_pressed() {
-			first_menu.show();
-		}
+	void display_2_pressed() {};
 
-		void quit() {
-			export_data();
-			hide();
-		};
+	void display_3_pressed() {};
 
-		//callback functions
-		static void cb_add(Address, Address);
-		static void cb_remove(Address, Address);
-		static void cb_request(Address, Address);
-		static void cb_display(Address, Address);
-		static void cb_menu(Address, Address);
-		static void cb_quit(Address, Address);
-		static void cb_add_customer(Address, Address);
-		static void cb_add_driver(Address, Address);
-		static void cb_add_place(Address, Address);
-		static void cb_add_submit(Address, Address);
-		static void cb_remove_customer(Address, Address);
-		static void cb_remove_driver(Address, Address);
-		static void cb_remove_place(Address, Address);
-		static void cb_remove_submit(Address, Address);
-		static void cb_request_submit(Address, Address);
-		static void cb_request_okay(Address, Address);
-		static void cb_display_customers_all(Address, Address);
-		static void cb_display_customers_neg(Address, Address);
-		static void cb_display_drivers_all(Address, Address);
-		static void cb_diplay_drivers_rad(Address, Address);
-	};
+	void display_4_pressed() {};
 
-<<<<<<< HEAD
-	User_window::User_window(Point xy, int w, int h, const string& title) :
-		//initialization
-		Window(xy, w, h, title),
-=======
+	void menu_pressed() {
+		first_menu.show();
+	}
+
+	void quit() {
+		export_data();
+		hide();
+	}
+
 	//callback functions
 	static void cb_add(Address, Address);
 	static void cb_remove(Address, Address);
@@ -365,334 +425,230 @@ private:
 	static void cb_remove_submit(Address, Address);
 	static void cb_request_submit(Address, Address);
 	static void cb_request_okay(Address, Address);
+	static void cb_display_customers_all(Address, Address);
+	static void cb_display_customers_neg(Address, Address);
+	static void cb_display_drivers_all(Address, Address);
+	static void cb_display_drivers_rad(Address, Address);
+	static void cb_display_places_all(Address, Address);
+	static void cb_display_places_withtag(Address, Address);
 	static void cb_2(Address, Address);
 	static void cb_3(Address, Address);
 	static void cb_4(Address, Address);
+
 };
 
 User_window::User_window(Point xy, int w, int h, const string& title) :
 	//initialization
 	Window(xy, w, h, title),
->>>>>>> 3fcbbf79fd4149c9cc61c6d604d5fc2590edb42c
 
-		quit_button(
-			Point(x_max() / 2 - 50, y_max() - 40),
-			100, 40,
-			"Quit",
-			cb_quit),
+	quit_button(
+		Point(x_max() / 2 - 50, y_max() - 40),
+		100, 40,
+		"Quit",
+		cb_quit),
 
-		first_menu(
-			Point(x_max() / 2 - 50, y_max() / 6),
-			100, 40,
-			Menu::vertical,
-			"My Ride"),
+	first_menu(
+		Point(x_max() / 2 - 50, y_max() / 6),
+		100, 40,
+		Menu::vertical,
+		"My Ride"),
 
-		add_customer_button(
-			Point(x_max() / 4 - 50, 50),
-			100, 40,
-			"Customer",
-			cb_add_customer),
+	add_customer_button(
+		Point(x_max() / 4 - 50, 50),
+		100, 40,
+		"Customer",
+		cb_add_customer),
 
-		add_driver_button(
-			Point(2 * x_max() / 4 - 50, 50),
-			100, 40,
-			"Driver",
-			cb_add_driver),
+	add_driver_button(
+		Point(2 * x_max() / 4 - 50, 50),
+		100, 40,
+		"Driver",
+		cb_add_driver),
 
-		add_place_button(
-			Point(3 * x_max() / 4 - 50, 50),
-			100, 40,
-			"Place",
-			cb_add_place),
+	add_place_button(
+		Point(3 * x_max() / 4 - 50, 50),
+		100, 40,
+		"Place",
+		cb_add_place),
 
-		add_submit(
-			Point(x_max() - 100, y_max() - 40),
-			100, 40,
-			"Submit",
-			cb_add_submit),
+	add_submit(
+		Point(x_max() - 100, y_max() - 40),
+		100, 40,
+		"Submit",
+		cb_add_submit),
 
-		add_name(
-			Point(x_max() / 2 - 50, 100),
-			100, 20,
-			"Name:"),
+	add_name(
+		Point(x_max() / 2 - 50, 100),
+		100, 20,
+		"Name:"),
 
-		add_balance(
-			Point(x_max() / 2 - 50, 130),
-			100, 20,
-			"Balance:"),
+	add_balance(
+		Point(x_max() / 2 - 50, 130),
+		100, 20,
+		"Balance:"),
 
-		add_driver_place(
-			Point(x_max() / 2 - 50, 160),
-			100, 20,
-			"Place:"),
+	add_driver_place(
+		Point(x_max() / 2 - 50, 160),
+		100, 20,
+		"Place:"),
 
-		add_lat(
-			Point(x_max() / 2 - 50, 190),
-			100, 20,
-			"Latitude:"),
+	add_lat(
+		Point(x_max() / 2 - 50, 190),
+		100, 20,
+		"Latitude:"),
 
-		add_lon(
-			Point(x_max() / 2 - 50, 220),
-			100, 20,
-			"Longitude:"),
+	add_lon(
+		Point(x_max() / 2 - 50, 220),
+		100, 20,
+		"Longitude:"),
 
-		add_tags(
-			Point(x_max() / 2 - 50, 250),
-			200, 20,
-			"Tags:"),
+	add_tags(
+		Point(x_max() / 2 - 50, 250),
+		200, 20,
+		"Tags:"),
 
-		add_photo_loc(
-			Point(x_max() / 2 - 50, 280),
-			100, 20,
-			"Photo filename:"),
+	add_photo_loc(
+		Point(x_max() / 2 - 50, 280),
+		100, 20,
+		"Photo filename:"),
 
-		add_type(
-			Point(x_max() - 100, y_max() - 100),
-			100, 20,
-			"Selected Type:"),
+	add_type(
+		Point(x_max() - 100, y_max() - 100),
+		100, 20,
+		"Selected Type:"),
 
-		remove_customer(
-			Point(x_max() / 4 - 50, 50),
-			100, 40,
-			"Customer",
-			cb_remove_customer),
+	remove_customer(
+		Point(x_max() / 4 - 50, 50),
+		100, 40,
+		"Customer",
+		cb_remove_customer),
 
-		remove_driver(
-			Point(2 * x_max() / 4 - 50, 50),
-			100, 40,
-			"Driver",
-			cb_remove_driver),
+	remove_driver(
+		Point(2 * x_max() / 4 - 50, 50),
+		100, 40,
+		"Driver",
+		cb_remove_driver),
 
-		remove_place(
-			Point(3 * x_max() / 4 - 50, 50),
-			100, 40,
-			"Place",
-			cb_remove_place),
+	remove_place(
+		Point(3 * x_max() / 4 - 50, 50),
+		100, 40,
+		"Place",
+		cb_remove_place),
 
-		remove_submit(
-			Point(x_max() - 100, y_max() - 40),
-			100, 40,
-			"Submit",
-			cb_remove_submit),
+	remove_submit(
+		Point(x_max() - 100, y_max() - 40),
+		100, 40,
+		"Submit",
+		cb_remove_submit),
 
-		remove_name(
-			Point(x_max() / 2 - 50, 100),
-			100, 20,
-			"Name:"),
+	remove_name(
+		Point(x_max() / 2 - 50, 100),
+		100, 20,
+		"Name:"),
 
-		request_placeA_name(
-			Point(x_max() / 2 - 100, 70),
-			200, 20,
-			"Place A:"),
+	request_placeA_name(
+		Point(x_max() / 2 - 100, 70),
+		200, 20,
+		"Place A:"),
 
-		request_customer_name(
-			Point(x_max() / 2 - 100, 40),
-			200, 20,
-			"Customer Name:"),
+	request_customer_name(
+		Point(x_max() / 2 - 100, 40),
+		200, 20,
+		"Customer Name:"),
 
-		request_placeB_name(
-			Point(x_max() / 2 - 100, 100),
-			200, 20,
-			"Place B:"),
+	request_placeB_name(
+		Point(x_max() / 2 - 100, 100),
+		200, 20,
+		"Place B:"),
 
-		request_info(
-			Point(x_max() - (x_max() - 100), 130),
-			x_max() - 200, y_max() - 230,
-			"Places  \nand  \ntags: "),
+	request_info(
+		Point(x_max() - (x_max() - 100), 130),
+		x_max() - 200, y_max() - 230,
+		"Places  \nand  \ntags: "),
 
-		request_submit(
-			Point(x_max() / 2 + 130, 40),
-			100, 80,
-			"Submit",
-			cb_request_submit),
+	request_submit(
+		Point(x_max() / 2 + 130, 40),
+		100, 80,
+		"Submit",
+		cb_request_submit),
 
-		request_okay(
-			Point(x_max() - 100, y_max() - 40),
-			100, 40,
-			"Okay",
-			cb_request_okay),
+	request_okay(
+		Point(x_max() - 100, y_max() - 40),
+		100, 40,
+		"Okay",
+		cb_request_okay),
 
-		request_summary(
-<<<<<<< HEAD
-			Point(x_max() - (x_max() - 20), 100),
-			x_max() - 40, 300,
-			""),
-
-		display_customers_all(
-			Point(150, 500), 100, 40, "All"
-		),
-
-		display_customers_neg(
-			Point(150, 550), 100, 40, "Customers with Negative Balance"
-		),
+	request_summary(
+		Point(x_max() - (x_max() - 20), 100),
+		x_max() - 40, 300,
+		""),
 
 
+	display_size_menu(
+		Point(x_max() / 2 - 50, y_max() / 6),
+		100, 40,
+		Menu::vertical,
+		"Select size"),
 
-	{
-		//constructor body
+	display_customers_all(
+		Point(50, 350),
+		120, 40,
+		"All Customers",
+		cb_display_customers_all),
 
-		attach(quit_button);
-		attach(add_customer_button);
-		attach(add_driver_button);
-		attach(add_place_button);
-		attach(add_submit);
-		attach(add_name);
-		attach(add_balance);
-		attach(add_driver_place);
-		attach(add_lat);
-		attach(add_lon);
-		attach(add_tags);
-		attach(add_photo_loc);
-		attach(add_type);
-		attach(remove_customer);
-		attach(remove_driver);
-		attach(remove_place);
-		attach(remove_submit);
-		attach(remove_name);
-		attach(request_placeA_name);
-		attach(request_placeB_name);
-		attach(request_customer_name);
-		attach(request_info);
-		attach(request_submit);
-		attach(request_okay);
-		attach(request_summary);
-		attach(display_customers_all);
-		attach(display_customers_neg);
+	display_customers_neg(
+		Point(50, 400),
+		120, 40,
+		"Neg Balance",
+		cb_display_customers_neg),
 
+	display_drivers_all(
+		Point(250, 350),
+		120, 40,
+		"All Drivers",
+		cb_display_drivers_all),
 
-		add_customer_button.hide();
-		add_driver_button.hide();
-		add_place_button.hide();
-		add_submit.hide();
-		add_name.hide();
-		add_balance.hide();
-		add_driver_place.hide();
-		add_lat.hide();
-		add_lon.hide();
-		add_tags.hide();
-		add_photo_loc.hide();
-		add_type.hide();
-		remove_customer.hide();
-		remove_driver.hide();
-		remove_place.hide();
-		remove_submit.hide();
-		remove_name.hide();
-		request_placeA_name.hide();
-		request_placeB_name.hide();
-		request_customer_name.hide();
-		request_info.hide();
-		request_submit.hide();
-		request_summary.hide();
-		request_okay.hide();
-		display_customers_all.hide();
-		display_customer_neg.hide();
+	display_drivers_tag(
+		Point(250, 400),
+		100, 20,
+		"Tag: "
+	),
 
-		first_menu.attach(new Button(Point(0, 0), 0, 0, "add", cb_add));
-		first_menu.attach(new Button(Point(0, 0), 0, 0, "remove", cb_remove));
-		first_menu.attach(new Button(Point(0, 0), 0, 0, "request", cb_request));
-		first_menu.attach(new Button(Point(0, 0), 0, 0, "display", cb_display));
-		attach(first_menu);
+	display_drivers_radius(
+		Point(250, 430),
+		100, 20,
+		"Radius: "
+	),
 
-	}
+	display_drivers_rad(
+		Point(250, 460),
+		100, 40,
+		"Within Radius",
+		cb_display_customers_neg),
 
-	//callback functions for buttons
-	void User_window::cb_quit(Address, Address pw) {
-		reference_to<User_window>(pw).quit();
-	}
+	display_places_all(
+		Point(450, 350),
+		120, 40,
+		"All Places",
+		cb_display_places_all
+	),
 
-	void User_window::cb_add(Address, Address pw) {
-		reference_to<User_window>(pw).add_pressed();
-	}
+	display_places_tag(
+		Point(450, 400),
+		100, 20,
+		"Tag: "
+	),
 
-	void User_window::cb_add_customer(Address, Address pw) {
-		reference_to<User_window>(pw).add_customer_pressed();
-	}
+	display_places_withtag(
+		Point(450, 430),
+		120, 40,
+		"With Tag",
+		cb_display_places_withtag
+	)
 
-	void User_window::cb_add_driver(Address, Address pw) {
-		reference_to<User_window>(pw).add_driver_pressed();
-	}
-
-	void User_window::cb_add_place(Address, Address pw) {
-		reference_to<User_window>(pw).add_place_pressed();
-	}
-
-	void User_window::cb_add_submit(Address, Address pw) {
-		reference_to<User_window>(pw).add_submit_pressed();
-	}
-
-	void User_window::cb_remove(Address, Address pw) {
-		reference_to<User_window>(pw).remove_pressed();
-	}
-
-	void User_window::cb_remove_customer(Address, Address pw) {
-		reference_to<User_window>(pw).remove_customer_pressed();
-	}
-
-	void User_window::cb_remove_driver(Address, Address pw) {
-		reference_to<User_window>(pw).remove_driver_pressed();
-	}
-
-	void User_window::cb_remove_place(Address, Address pw) {
-		reference_to<User_window>(pw).remove_place_pressed();
-	}
-
-	void User_window::cb_remove_submit(Address, Address pw) {
-		reference_to<User_window>(pw).remove_submit_pressed();
-	}
-
-	void User_window::cb_request(Address, Address pw) {
-		reference_to<User_window>(pw).request_pressed();
-	}
-
-	void User_window::cb_request_submit(Address, Address pw) {
-		reference_to<User_window>(pw).request_submit_pressed();
-	}
-
-	void User_window::cb_request_okay(Address, Address pw) {
-		reference_to<User_window>(pw).request_okay_pressed();
-	}
-
-	void User_window::cb_display(Address, Address pw) {
-		reference_to<User_window>(pw).display_pressed();
-	}
-
-	void User_window::cb_display_customers_all(Address, Address pw) {
-		reference_to<User_window>(pw).display_customers_all_pressed();
-	}
-
-	void User_window::cb_display_customers_neg(Address, Address pw) {
-		reference_to<User_window>(pw).display_customers_neg_pressed();
-	}
-
-	void User_window::cb_menu(Address, Address pw) {
-		reference_to<User_window>(pw).menu_pressed();
-	}
-
-	int main() {
-		try {
-			import_data();
-			User_window win(Point(100, 100), 600, 600, "My Ride");
-			return gui_main();
-		}
-		catch (exception& e) {
-			cerr << "exception: " << e.what() << '\n';
-			return 1;
-		}
-	}
-=======
-			Point(x_max() - (x_max()-20), 100),
-			x_max()-40, 300,
-			""),
-
-		display_size_menu(
-			Point(x_max() / 2 - 50, y_max() / 6),
-			100, 40,
-			Menu::vertical,
-			"Select size")
 
 {
 	//constructor body
-
 	attach(quit_button);
 	attach(add_customer_button);
 	attach(add_driver_button);
@@ -718,6 +674,15 @@ User_window::User_window(Point xy, int w, int h, const string& title) :
 	attach(request_submit);
 	attach(request_okay);
 	attach(request_summary);
+	attach(display_customers_all);
+	attach(display_customers_neg);
+	attach(display_drivers_all);
+	attach(display_drivers_rad);
+	attach(display_drivers_tag);
+	attach(display_drivers_radius);
+	attach(display_places_all);
+	attach(display_places_tag);
+	attach(display_places_withtag);
 
 
 	add_customer_button.hide();
@@ -744,6 +709,15 @@ User_window::User_window(Point xy, int w, int h, const string& title) :
 	request_submit.hide();
 	request_summary.hide();
 	request_okay.hide();
+	display_customers_all.hide();
+	display_customers_neg.hide();
+	display_drivers_all.hide();
+	display_drivers_rad.hide();
+	display_drivers_radius.hide();
+	display_drivers_tag.hide();
+	display_places_all.hide();
+	display_places_tag.hide();
+	display_places_withtag.hide();
 
 	display_size_menu.attach(new Button(Point(0, 0), 0, 0, "2 x 2", cb_2));
 	display_size_menu.attach(new Button(Point(0, 0), 0, 0, "3 x 3", cb_3));
@@ -820,20 +794,40 @@ void User_window::cb_display(Address, Address pw) {
 	reference_to<User_window>(pw).display_pressed();
 }
 
-void User_window::cb_display(Address, Address pw) {
-	//reference to display pressed
+void User_window::cb_display_customers_all(Address, Address pw) {
+	reference_to<User_window>(pw).display_customers_all_pressed();
+}
+
+void User_window::cb_display_customers_neg(Address, Address pw) {
+	reference_to<User_window>(pw).display_customers_neg_pressed();
+}
+
+void User_window::cb_display_drivers_all(Address, Address pw) {
+	reference_to<User_window>(pw).display_drivers_all_pressed();
+}
+
+void User_window::cb_display_drivers_rad(Address, Address pw) {
+	reference_to<User_window>(pw).display_drivers_rad_pressed();
+}
+
+void User_window::cb_display_places_all(Address, Address pw) {
+	reference_to<User_window>(pw).display_places_all_pressed();
+}
+
+void User_window::cb_display_places_withtag(Address, Address pw) {
+	reference_to<User_window>(pw).display_places_withtag_pressed();
 }
 
 void User_window::cb_2(Address, Address pw) {
-	// 2x2 display pressed
+	reference_to<User_window>(pw).display_2_pressed();
 }
 
 void User_window::cb_3(Address, Address pw) {
-	// 3x3 display pressed
+	reference_to<User_window>(pw).display_3_pressed();
 }
 
 void User_window::cb_4(Address, Address pw) {
-	// 4x4 display pressed
+	reference_to<User_window>(pw).display_4_pressed();
 }
 
 void User_window::cb_menu(Address, Address pw) {
@@ -851,4 +845,3 @@ int main() {
 		return 1;
 	}
 }
->>>>>>> 3fcbbf79fd4149c9cc61c6d604d5fc2590edb42c
